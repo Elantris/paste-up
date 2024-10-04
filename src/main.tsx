c533e8a1190a28f3750e62da1c781e75581ebe6f
Elantris <j88107333@gmail.com>
2024-10-05 02:23:56 +0800

    Add tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
+import "./index.css"
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import {
+  isolateInsideOfContainer,
+  scopedPreflightStyles,
+} from "tailwindcss-scoped-preflight"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // ...
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer(".twp"),
+    }),
+  ],
+}
