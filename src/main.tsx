a526783a008dfd951f027c59c0154199b7298429
Elantris <j88107333@gmail.com>
2024-10-04 22:06:09 +0800

    Add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+}
+const theme = extendTheme({ config })
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
